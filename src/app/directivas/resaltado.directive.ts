dfa1057a6944bbcfb15ca7c3ee2d6ab7f14f1040
Juan Diego Gómez <juandieruiz6@gmail.com>
2022-01-21 19:37:50 +0100

    documentation directive

--- src/app/directivas/resaltado.directive.ts
+++ src/app/directivas/resaltado.directive.ts
@@ -5,18 +5,19 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class ResaltadoDirective {
 
-  constructor(public el: ElementRef) {
+  
+  constructor(public el: ElementRef) { // el es una variable que se crea en el constructor
     
     } 
 
-    ngOnInit(){
-      let element = this.el.nativeElement;
-      element.style.backgroundColor = 'blue';
-      element.style.color = 'white';
-      element.style.padding = '20px';
-      element.style.margin = '20px';0
-      element.style.border = '1px solid black';
+    ngOnInit(){ // se ejecuta cuando se inicia el componente
+      let element = this.el.nativeElement; // el.nativeElement es el elemento que se esta resaltando
+      element.style.backgroundColor = 'blue'; // cambiando el color de fondo
+      element.style.color = 'white';  // cambiando el color del texto
+      element.style.padding = '20px';  // agregando padding
+      element.style.margin = '20px';0   // agregando margen
+      element.style.border = '1px solid black'; // agregando borde
 
-      element.innerText = element.innerText.toUpperCase();
+      element.innerText = element.innerText.toUpperCase(); // cambiando el texto a mayusculas
     }
 }
