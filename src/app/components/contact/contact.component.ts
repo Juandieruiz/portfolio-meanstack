d3a3f8486236feb3880e90409b8275fc7a7bbb3a
Juan Diego Gómez <juandieruiz6@gmail.com>
2022-01-21 17:18:57 +0100

    Viewchilds

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  @ViewChild('textos', {static: true}) textos:any; // tomando elementos con Angular
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // tomando elementos con JavaScript
+    //let opcion_clasica = alert((document.querySelector('#texto')?.innerHTML));
+    // tomando elementos con Angular
+    console.log(this.textos.nativeElement.innerHTML);
   }
 
 }
